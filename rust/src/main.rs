ce950e916a5e0b6cf9a0469db3476f44d6178d49
Martin <martin@automata.tech>
2023-08-01 16:22:36 +0100

    feat(rust): add stamina and idle_time to state

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,24 +1,37 @@
 use crate::engine::{actions, Action};
-use std::io::Write;
+use std::{io::Write, time::Instant};
 
 mod engine;
 
 pub struct State {
     distance: u32,
+    stamina: i32,
+    idle_time: Instant,
 }
 
-fn walk(state: &mut State) {
-    state.distance += 1;
+fn move_player(state: &mut State, distance: u32, stamina: i32) {
+    if state.stamina < stamina {
+        println!("You are too tired to move");
+        return;
+    }
+    state.distance += distance;
+    state.stamina -= stamina;
+    state.idle_time = Instant::now();
 }
 
 actions! {
-    WALK => (1, "Walk", "Walking", Some(walk)),
-    RUN => (2, "Run", "Running", Some(|state| state.distance += 2)),
+    WALK => (1, "Walk", "Walking", Some(|state| move_player(state, 1, 1))),
+    RUN => (2, "Run", "Running", Some(|state| move_player(state, 3, 2))),
+    JUMP => (3, "Jump", "Jumping", Some(|state| move_player(state, 2, 3))),
     END => (0, "End", "Ending", None)
 }
 
 fn main() {
-    let mut state = State { distance: 0 };
+    let mut state = State {
+        distance: 0,
+        stamina: 10,
+        idle_time: Instant::now(), // now
+    };
 
     loop {
         println!("{}", MENU);
@@ -32,6 +45,9 @@ fn main() {
             Ok(num) => {
                 match get_action(num) {
                     Some(action) => {
+                        let elapsed = state.idle_time.elapsed();
+                        state.stamina += elapsed.as_secs() as i32;
+
                         match action.key {
                             ActionKeys::END => {
                                 break;
@@ -56,5 +72,6 @@ fn main() {
         }
 
         println!("Distance: {}", state.distance);
+        println!("Stamina: {}", state.stamina);
     }
 }
